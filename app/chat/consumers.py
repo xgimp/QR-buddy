3a71510fc33b78edac5a00bb79d6c03a021efc9f
Lukas Bacovsky <l.bacovsky@gmail.com>
2024-07-19 17:39:58 +0200

    save chat messages

--- app/chat/consumers.py
+++ app/chat/consumers.py
@@ -1,10 +1,20 @@
 import json
 
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+
+from chat.models import Message
+from qr_pair.models import ChatRoom, QRCode
 
 
 # https://stackoverflow.com/questions/64188904/django-channels-save-messages-to-database
 class ChatConsumer(AsyncWebsocketConsumer):
+    @database_sync_to_async
+    def create_chat(self, message, sender):
+        chat_room = ChatRoom.objects.get(id=self.room_name)
+        sndr = QRCode.objects.get(id=sender, chat_room=chat_room)
+        return Message.objects.create(sender=sndr, message=message)
+
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
@@ -38,9 +48,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
         """
         Receive message from room group
         """
+
+        # It is necessary to await creation of messages
+        new_msg = await self.create_chat(
+            message=event["message"], sender=event["sender"]
+        )
         # Send message to WebSocket
         await self.send(
             text_data=json.dumps(
-                {"message": event["message"], "sender": event["sender"]}
+                {"message": new_msg.message, "sender": str(new_msg.sender)}
             )
         )
---
+++ app/chat/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Message(models.Model):
+    message = models.TextField()
+    sender = models.ForeignKey("qr_pair.QRCode", on_delete=models.CASCADE)
+    sent_at = models.DateTimeField(auto_now_add=True)
