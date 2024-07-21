7b8b3d74d274e9c527eae0c07cce14a8f7407225
Lukas Bacovsky <l.bacovsky@gmail.com>
2024-07-21 09:48:53 +0200

    LoginRequired

--- app/qr_pair/views.py
+++ app/qr_pair/views.py
@@ -1,11 +1,12 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView, ListView
 
 from qr_pair.models import ChatRoom
 
 
-class ChatRoomDetailView(DetailView):
+class ChatRoomDetailView(LoginRequiredMixin, DetailView):
     model = ChatRoom
 
 
-class ChatRoomListView(ListView):
+class ChatRoomListView(LoginRequiredMixin, ListView):
     model = ChatRoom
