7d9d1b67338f709e8bf8c6092a62b8cb2b198bae
Lukas Bacovsky <l.bacovsky@gmail.com>
2024-07-26 17:59:09 +0200

    url changed

--- app/chat/urls.py
+++ app/chat/urls.py
@@ -4,6 +4,6 @@ from .views import room, SendMessage
 
 app_name = "chat"
 urlpatterns = [
-    path("<str:room_name>/<str:user_id>", room, name="room"),
+    path("<str:room_name>/<str:user_id>/", room, name="room"),
     path("send", SendMessage.as_view(), name="send_message"),
 ]
