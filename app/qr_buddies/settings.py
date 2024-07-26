6dc2c24e1ed8d98554f649e02aceaba9516772a6
Lukas Bacovsky <l.bacovsky@gmail.com>
2024-07-26 21:26:17 +0200

    revert: STATIC_ROOT path

--- app/qr_buddies/settings.py
+++ app/qr_buddies/settings.py
@@ -139,8 +139,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = PROJECT_DIR / "static"
+STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = "static/"
+
 
 STATICFILES_DIRS = [
     PROJECT_DIR / "static",
