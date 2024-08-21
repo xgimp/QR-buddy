cabebb52a3b66285a766fdc7e22327da7aec9364
Lukáš Bačovský <l.bacovsky@gmail.com>
2024-08-21 18:43:12 +0200

    added simple administration [WIP, missing tests]

--- app/administration/urls.py
+++ app/administration/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import RoomListView
-
+from .views import RoomListView, RoomCreateView, RoomDeleteView
 
 app_name = "administration"
 urlpatterns = [
-    path("rooms", RoomListView.as_view(), name="room_list"),
+    path("", RoomListView.as_view(), name="room_list"),
+    path("rooms/add", RoomCreateView.as_view(), name="create_room"),
+    path("rooms/delete/<str:room_pk>/", RoomDeleteView.as_view(), name="delete_room"),
 ]
--- app/administration/views.py
+++ app/administration/views.py
@@ -1,7 +1,21 @@
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView
 
 from qr_pair.models import ChatRoom
+from django.urls import reverse_lazy
 
 
 class RoomListView(ListView):
     model = ChatRoom
+
+
+class RoomCreateView(CreateView):
+    model = ChatRoom
+    fields = "__all__"
+    success_url = reverse_lazy("administration:room_list")
+
+
+class RoomDeleteView(DeleteView):
+    model = ChatRoom
+    pk_url_kwarg = "room_pk"
+    success_url = reverse_lazy("administration:room_list")
